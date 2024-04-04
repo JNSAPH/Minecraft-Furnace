9458a5c118cc9d46d27dd79525d6e31b1b05c898
Jonas <JNSAPH@icloud.com>
2024-04-04 19:49:31 +0200

    Turned off react/no-unescaped-entities lint rule

--- www/src/components/tutorialTerminal/tutorialTerminal.tsx
+++ www/src/components/tutorialTerminal/tutorialTerminal.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface TutorialTerminalProps {
     step: 1 | 2 | 3;
 }
@@ -6,8 +8,10 @@ const TutorialTerminal = ({ step }: TutorialTerminalProps) => {
     return (
         <div className="relative select-none">
             <div className="w-full max-w-[500px] mx-auto">
-                <img
-                src={`terminal_step${step}.png`} 
+                <Image
+                width={577}
+                height={385}
+                src={`/terminal_step${step}.png`} 
                 alt={`Image representing step ${step} in the terminal`}
                 className="w-full pointer-events-none" />
             </div>
